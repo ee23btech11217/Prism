8eac01940a746baac25ea29ca4bc3fe93a0d415d
Mihir-Divyansh <jee.emd2005@gmail.com>
2025-02-09 06:40:19 +0530

    Added Folder Structure for Reference

--- placeholders.py
+++ placeholders.py
@@ -2,8 +2,10 @@ import os
 import subprocess
 
 for roots, dirs, files in os.walk('.'):
-    if not dirs:
-        subprocess.run(["touch", ".placeholder"])
+    if not dirs and not files:
+        path = os.path.join(roots, ".placeholder")
 
-
+        with open(path, 'w') as pl:
+            pl.write("Adding folder structure to git")
+        print(roots)
 
